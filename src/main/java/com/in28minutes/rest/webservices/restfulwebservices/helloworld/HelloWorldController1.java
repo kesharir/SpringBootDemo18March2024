1f5f52457d29d394e6ff588d53f7e562f4fb24e1
kesharir <ritanshu.keshari@gmail.com>
2024-03-18 14:31:50 +0530

    Understanding RequestMapping vs GetMapping

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController1.java
@@ -0,0 +1,12 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController1 {
+    @GetMapping(path = "hello-world1")
+    public String helloWorld1() {
+        return "Hello World 1";
+    }
+}
