c2b395859bd39fb13637dbec7fe08630087dc9d7
kesharir <ritanshu.keshari@gmail.com>
2024-03-22 19:19:49 +0530

    Content Negotiation :Internationalization i18n by using RequestHeader : "Accept-Language"

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/advancedRestAPI/contentNegotitation/theory.md
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/advancedRestAPI/contentNegotitation/theory.md
@@ -8,3 +8,14 @@
   - Content Negotiation
 - Example: Accept header (MIME types - application/xml, application/json .....)
 - Example: Accept-Language header (en, nl, fr...)
+
+## Internationalization - i18n
+
+- Your REST API might have consumers from around the world.
+- How do ou customize it to users around the world?
+  - Internationalization - i18n
+- Typically HTTP Request Header - Accept-Language is used
+  - Accept-Language - indicated natural language and locale that the consumer prefers
+  - Example: en - English (Good Morning)
+  - Example: nl - Dutch(Goedemorgen)
+
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController1.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController1.java
@@ -1,11 +1,22 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController1 {
+    private MessageSource messageSource;
+
+    public HelloWorldController1(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "hello-world1")
     public String helloWorld1() {
         return "Hello World 1";
@@ -22,5 +33,11 @@ public class HelloWorldController1 {
     @GetMapping(path = "hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello World :" + name);
+    }
+
+    @GetMapping(path = "hello-world1-internationalized")
+    public String helloWorld1Internationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
